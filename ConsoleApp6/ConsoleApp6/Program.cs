1636fee978581df970ac7fd710b82fd7f38ff9ba
Mresu <161798697+Mresu@users.noreply.github.com>
2024-03-26 23:56:49 +0400

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Channels;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Vehicle Vehicle=new Vehicle("BMW", "X5", 500000);
+            
+        }
+    }
+    class Vehicle
+    {
+        public string Brand;
+        public string Model;
+        public double Mile;
+            public Vehicle( string brand,  string model, double mile )
+        {
+            Brand = brand;
+            Model = model;
+            
+        }
+        public void ShowFullName()
+        {
+            Console.WriteLine(Brand+" "+Model);
+        }
+    
+
+        
+    }
+    class Car:Vehicle
+    {
+        int DoorsNum;
+        Car(string brand, string model, double mile):base(brand, model, mile)
+        {
+            
+            DoorsNum = 5;
+
+        }
+        public void ShowFullInfo()
+        {
+            Console.WriteLine(Brand+" "+" "+Model+" "+Mile+" "+DoorsNum);
+        }
+
+    }
+    class MotorCycle : Vehicle 
+    {
+        public int WheelNum;
+        
+        MotorCycle(string brand, string model, double mile):base(brand, model, mile)
+        {
+            this.WheelNum = 8;
+        }
+        public void ShowFullInfo()
+        {
+            Console.WriteLine(this.WheelNum);
+
+        }
+    }
+
+}
